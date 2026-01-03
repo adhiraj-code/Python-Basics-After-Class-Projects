ab9fb163fae3afb3a6703137ace389bb592c8ab2
adhiraj-code <alkarathore87@gmail.com>
2026-01-03 22:50:11 +0530

    Add files via upload

---
+++ StoringBirthdays.py
@@ -0,0 +1,14 @@
+rohit = "12th February"
+print("Rohit's Birthday: ", rohit)
+
+rohan = "5th May"
+print("Rohan's Birthday: ", rohan)
+
+priya = "9th Novembor"
+print("Priya's Birthday: ", priya)
+
+rishi = "5th June"
+print("Rishi's Birthday: ", rishi)
+
+vivaan = "7th September"
+print("Vivaan's Birthday: ", vivaan)
